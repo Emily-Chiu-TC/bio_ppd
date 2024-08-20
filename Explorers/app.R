dbea373a95f10c927dd8b84013f8348595c2838c
Emily-Chiu-TC <emilyuk2022@gmail.com>
2024-08-20 21:04:21 +0000

    tidied the script for easier searches. will un-hash the script-loading lines once I figure out how to load the previously collected data into the dashboard graphical output / how the dashboard can communicate with an object/csv file for data storage

--- Explorers/app.R
+++ Explorers/app.R
@@ -9,45 +9,49 @@ library(RColorBrewer)
 library(colorBlindness)
 
 # scripts----
-source("scripts/marble.R")
-source("scripts/sci_fest_clean.R")
+#source("scripts/marble.R")
+#source("scripts/sci_fest_clean.R")
 
 # Define UI----
+## sidebar----
 ui <- page_sidebar(
   title = "BIO PPD: Data Explorers (2024)",
   sidebar = sidebar(code(print("This is R, one of the programming languages used by biologists.")),
-                    
+### text information----                    
                     card(card_header("Information"),
                          helpText("Note: Input a numerical guess for the number of marble,",
                                   "pick your favourite science subject,",
                                   "and adjust the colours and the features for the plots."),
-                         
+### date input----                       
                          dateInput("date", label = "Select date", value = "2024-07-14")),
-                    
+### marble input----                    
                     card(card_header("Marble Guess"),
                          numericInput("marble", "Input number", value = 1), selected = 1),
-                    
+### subject input----                    
                     card(card_header("Favourite Science Subject"),
                          radioButtons("radio", label = "Select one subject",
                                       choices = list("Biology", "Chemistry", 
                                                      "Physics", "Mathematics")),
-                         
+### graph colour input (tbc)----                         
                          helpText("Now select the colours for the graph"),
                          
                          radioButtons('bio', label = "Biology",
                                       choices = list("red", "orange", "yellow"))
                     
                     )),
-                    
+## main page----
+### number of visitors (tbc on number of input)----
   value_box( #can this be smaller?
-    title = "Engagement",
-    value = 100,
+    title = "Dashboard Visitors",
+    value = 1,
     showcase = bsicons::bs_icon("bar-chart"), size = 1,
     theme = "teal"),
-  
+
+### subject----  
   card(card_header("Favourite Science Subject"),
        textOutput("selected_radio")),
-  
+
+### marble----  
   card(card_header("Your Marble Guess"),
        textOutput("selected_marble"))
 
@@ -55,10 +59,12 @@ ui <- page_sidebar(
   
 # Define server logic----
 server <- function(input, output) {
+## subject output (tbc)----
   output$selected_radio <- renderText({
     paste("You have selected: ", input$radio)
   })
-  
+
+## marble output (tbc) ----
   output$selected_marble <- renderText({
     paste("Your guess is:", input$marble)
   })
